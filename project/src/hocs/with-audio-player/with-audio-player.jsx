1c1c139a98050eea31b4494d963464a34b7fd2f0
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-15 14:51:08 +0300

    4.6. Добавит HOC withAudioPlayer.
    Создадим директорию hocks, а в ней новый модуль `with-audio-player.jsx`.
    В нём опишем одноимённый HOC.
    
    В этом HOC реализуем стейт для хранения информации о плеере, который
    воспроизводит мелодию в настоящий момент времени. HOC вернёт новый
    компонент и через render-prop прокинет `AudioPlayer`.

---
+++ project/src/hocs/with-audio-player/with-audio-player.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import AudioPlayer from '../../components/audio-player/audio-player';
+
+const withActivePlayer = (Component) => {
+  function WithActivePlayer(props) {
+    const [activePlayerId, setActivePlayerId] = useState(0);
+    return (
+      <Component
+        {...props}
+        renderPlayer={(src, id) => (
+          <AudioPlayer
+            src={src}
+            isPlaying={id === activePlayerId}
+            onPlayButtonClick={() => {
+              setActivePlayerId(activePlayerId === id ? -1 : id);
+            }}
+          />
+        )}
+      />
+    );
+  }
+
+  return WithActivePlayer;
+};
+
+export default withActivePlayer;
